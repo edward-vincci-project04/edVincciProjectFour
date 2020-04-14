05c4a0a6ab7e55bcc2894d14c285d8656752b867
vt1022 <57807628+vt1022@users.noreply.github.com>
2020-04-14 17:36:30 -0400

    adding icons for landing. 3 colors in variablesMixins.scss. and html for landing ᓚᘏᗢ

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,17 @@
  * destinations will be put into a list on the page.  * 
  * 
  * 
-  Initial travel list to work with (subject to change and/or grow):
-  Bali, Indonesia
-  Kerry, Ireland
-  The Maldives
-  Waikato, New Zealand
-  McMurdo Station, Antarctica
-  Bridgetown, Barbados
-  Providencia, Colombia
-  Reykjavík, Iceland
-  Kyoto, Japan
-  Tromsø, Norway
+    Initial travel list to work with (subject to change and/or grow):
+    Bali, Indonesia
+    Kerry, Ireland
+    The Maldives
+    Waikato, New Zealand
+    McMurdo Station, Antarctica
+    Bridgetown, Barbados
+    Providencia, Colombia
+    Reykjavík, Iceland
+    Kyoto, Japan
+    Tromsø, Norway
  * 
  * Stretch goals:
  * 
