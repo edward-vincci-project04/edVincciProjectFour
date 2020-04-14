0d59cfd0cdefccbd8ef9268a8e486756c216d100
Edward Boland <mr.eboland@gmail.com>
2020-04-14 12:43:40 -0400

    pseudo code for project4

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+/**
+ *  Destination places by Weather
+ * 
+ * Landing page welcoming the user and explaining what the app does.
+ * A button which will allow the user to proceed. Said button will have a listener that will hide or scroll the welcome screen to the main content.
+ * 
+ * Main content of page will have a 'box' with pictures of weather preferences (Cold, warm, hot).
+ * The imgs will be selectors or maybe using a click listener give the action an animation like a button to show it being selected.
+ * 
+ * The selection will populate travel destinations using ajax calls from which we will pull data out based on the temperature range the user selected. Those 
+ * destinations will be pulled to a list on the page.  * 
+ * 
+ * 
+ * Stretch goals:
+ * 
+ * From the list the user can select a destination which will pull it up on the map.
+ * 5 or 7 day forecast of location selected
+ * top restaurants in the location selected
+ * Recommended hotels in the location selected
+ * selector for weather condition to further refine search
+ * 
+ * 
+ */
+
+
 $(()=> {
   console.log('doc ready test');
 })
