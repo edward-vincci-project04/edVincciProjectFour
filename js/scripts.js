1025df368e3e4668a47038258c73ab82307561fe
vt1022 <57807628+vt1022@users.noreply.github.com>
2020-04-16 00:01:55 -0400

    adding cityInfo onto page. api stopped working. not getting response from postman. goodnight i guess. (_　_)。゜zｚＺ

--- js/scripts.js
+++ js/scripts.js
@@ -22,17 +22,6 @@
     Reykjavík, Iceland
     Kyoto, Japan
     Tromsø, Norway
-    Initial travel list to work with (subject to change and/or grow):
-    Bali, Indonesia
-    Kerry, Ireland
-    The Maldives
-    Waikato, New Zealand
-    McMurdo Station, Antarctica
-    Bridgetown, Barbados
-    Providencia, Colombia
-    Reykjavík, Iceland
-    Kyoto, Japan
-    Tromsø, Norway
  * 
  * Stretch goals:
  * 
@@ -52,6 +41,7 @@ const vacayApp = {};
 vacayApp.hereApiKey = `Cl4BqeFBq-GNBKFZC1Nz9Ux12AiOXdtj6r2EG-CWSdY`;
 vacayApp.hereURL = `https:weather.ls.hereapi.com/weather/1.0/report.json`;
 vacayApp.hereMapURL = `https://image.maps.ls.hereapi.com/mia/1.6/`;
+vacayApp.userSelect;
 vacayApp.destinations = [
     ["Bali", "You’ll find beaches, volcanoes, Komodo dragons and jungles sheltering elephants, orangutans and tigers. Basically, it’s paradise. It’s likely you’ve seen an image of Bali on social media at least once in the past seven days, as it’s such a popular bucket list destination for 2019. -forbes.com"], 
     ["Kerry", "All the way west in Ireland is one of the country’s most scenic counties. Kerry’s mountains, lakes and coasts are postcard-perfect, and that’s before you add in Killarney National Park. The unique small towns such as Dingle add to its charm. -forbes.com"], 
@@ -59,28 +49,33 @@ vacayApp.destinations = [
     ["Waikato", "Waikato, a region in New Zealand’s North Island, is home to massive underground caves, lush rainforest and the buzzy city of Hamilton. But the area’s main attraction? A Middle-earth adventure on the film set of Lord of the Rings. Hobbiton Movie Set still has the original Hobbit holes from the making of the films. -forbes.com"], 
     ["Mcmurdo Station", "The McMurdo Station is a United States Antarctic research station on the south tip of Ross Island, which is in the New Zealand–claimed Ross Dependency on the shore of McMurdo Sound in Antarctica. It is operated by the United States through the United States Antarctic Program, a branch of the National Science Foundation. -wikipedia.org"],
     ["Bridgetown", "Bridgetown, the capital of Barbados, is a port city on the island’s southwest coast. It's known for its British colonial architecture, 17th-century Garrison and horseracing track. Near the central National Heroes Square, which fringes the Constitution River, Nidhe Israel Synagogue and its museum explore the island’s Jewish history. -wikipedia.org"], 
-    ["Providencia", "Providencia is an ecologically important reserve that contains some of the world's greatest marine biodiversity, and incorporates the Archipelago of San Andrés, Providencia and Santa Catalina, in addition to the remote uninhabited cays at Roncador Bank, Serrana Bank, as well as distant reefs that include Quita Sueño Bank, Rosalind Bank and Alice Shoal. -wikipedia.org"], 
+    ["Marrakesh", "This ancient walled city is home to mosques, palaces and lush gardens. It’s known as The Red City thanks to the color of the brick walls surrounding the city. The medina is a UNESCO World Heritage Centre. -forbes.com"], 
     ["Reykjavik", "The first permanent settlement in Iceland by Norsemen is believed to have been established at Reykjavík by Ingólfr Arnarson around AD 870; this is described in Landnámabók, or the Book of Settlement. Ingólfur is said to have decided the location of his settlement using a traditional Norse method: he cast his high seat pillars (Öndvegissúlur) into the ocean when he saw the coastline, then settled where the pillars came to shore. -wikipedia.org"], 
     ["Kyoto", "About 20% of Japan's National Treasures and 14% of Important Cultural Properties exist in the city proper. The UNESCO World Heritage Site Historic Monuments of Ancient Kyoto (Kyoto, Uji and Otsu Cities) includes 17 locations in Kyoto, Uji in Kyoto Prefecture, and Ōtsu in Shiga Prefecture. The site was designated as World Heritage in 1994. -wikipedia.org"], 
-    ["Tromso", "The city centre of Tromsø contains the highest number of old wooden houses in Northern Norway, the oldest house dating from 1789. The city is a cultural centre for its region, with several festivals taking place in the summer. Torbjørn Brundtland and Svein Berge of the electronica duo Röyksopp and Lene Marlin grew up and started their careers in Tromsø. Noted electronic musician Geir Jenssen also hails from Tromsø. -wikipedia.org"]];
+    ["Tromso", "The city centre of Tromsø contains the highest number of old wooden houses in Northern Norway, the oldest house dating from 1789. The city is a cultural centre for its region, with several festivals taking place in the summer. Torbjørn Brundtland and Svein Berge of the electronica duo Röyksopp and Lene Marlin grew up and started their careers in Tromsø. -wikipedia.org"]];
 
-vacayApp.userSelect;
 
 // loop through destinations individually to feed into ajax
 vacayApp.destinationsCycle = () => {
     vacayApp.destinations.forEach((location) => {
-        vacayApp.getDestWeather(location[0]);
+        // vacayApp.getDestWeather(location[0]);
+        // replaced above with more reusable promise
+        vacayApp.weatherPromise(location[0], "observation").then( (result) => {
+            const vacayArray = result.observations.location[0];
+            vacayApp.displayVacay(vacayArray, vacayApp.userSelect, location[1]);
+            
+        });
     });
 };
 
 // display the vacations onto the page 
-vacayApp.displayVacay = (vacay, userSelection) => { //this needs to take data from two different places
+vacayApp.displayVacay = (vacay, userSelection, cityInfo) => { //this needs to take data from two different places
     // display city
     const city = vacay.city;
     // disp country
     const country = vacay.country;
-    // disp weather
-    const temp = Math.floor(vacay.observation[0].highTemperature);
+    // disp weather (average temperature)
+    const temp = Math.floor((parseInt(vacay.observation[0].highTemperature) + parseInt(vacay.observation[0].lowTemperature)) / 2);
     // Weather api has a weather description which can be helpful for our needs
     const tempDesc = vacay.observation[0].temperatureDesc;
 
@@ -95,20 +90,27 @@ vacayApp.displayVacay = (vacay, userSelection) => { //this needs to take data fr
         // console.log(`The weather in ${city}, ${country} is ${tempDesc}. The current high temperature is ${temp}.`);
         // need to append the above to the html when that portion has been completed
         
-        vacayApp.mapPromise(city).then( (result) => {
-            // trying to display map image but the responseText is literally an image. don't know how to use it
+        // vacayApp.mapPromise(city).then( (result) => {
+        //     // trying to display map image but the responseText is literally an image. don't know how to use it
+        // });
+        
+        vacayApp.weatherPromise(city, "forecast_7days").then( (result) => {
+            const forecastsArray = result.forecasts.forecastLocation.forecast;
+            console.log(forecastsArray);
+            
+            // Povidencia is 26C but is cool. ???
+            const resultsHtml = `
+                <button class="${city} ${country}">${city}, ${country}</button>
+                <p>The current average temperature is ${temp}</p>
+                <p>${cityInfo}</p>`;
+            
+            $(".displayResults").append(resultsHtml);
         });
-        
-        const resultsHtml = `
-            <button class="${city} ${country}">${city}, ${country}</button>
-            <p>The current high temperature is ${temp}</p>`;
-        
-        $(".displayResults").append(resultsHtml);
 
     } else if (userSelection != tempDesc) {
         // nothing happens if the temperature doesn't matech the selected temp
     } else {
-        console.log("FUUUUCCCK"); // error
+        console.log("type while (safi is old) console.log('fuuuuuck')"); // error
     }
 
 }
@@ -132,7 +134,23 @@ vacayApp.getDestWeather = (input) => {
     });
 };
 
-// maps promise
+// is it possible to make above more reusable by saving into promise?n
+vacayApp.weatherPromise = (city, product) => {
+    return $.ajax({
+        url: vacayApp.hereURL,
+        method: "GET",
+        dataType: "json",
+        data: {
+            apiKey: vacayApp.hereApiKey,
+            product: product,
+            name: city,
+            nocp: true
+        }
+    });
+}
+
+// maps promise for later.
+// maybe we should cut the map. they look so ugly
 vacayApp.mapPromise = (city) => {
     return $.ajax({
         url: vacayApp.hereMapURL,
@@ -153,7 +171,7 @@ vacayApp.mapPromise = (city) => {
 // -------------------
 vacayApp.init = () => {
     // user select portion
-    $(".imgSelection").on("click", function () {
+    $(".imgSelection").on("click", function() {
         
         vacayApp.userSelect = $(this).val();
         vacayApp.destinationsCycle();
