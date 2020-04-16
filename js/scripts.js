c8541bfd58afc51990ea967233beca039c89d41e
vt1022 <57807628+vt1022@users.noreply.github.com>
2020-04-15 22:05:10 -0400

    adding descriptions for each location. trying to add a map display

--- js/scripts.js
+++ js/scripts.js
@@ -50,32 +50,26 @@ const vacayApp = {};
 
 // variables
 vacayApp.hereApiKey = `Cl4BqeFBq-GNBKFZC1Nz9Ux12AiOXdtj6r2EG-CWSdY`;
-vacayApp.hereURL = `https:weather.ls.hereapi.com/weather/1.0/report.json`
-vacayApp.destinations = ["Bali", "Kerry", "Male", "Waikato", "Mcmurdo Station", "Bridgetown", "Providencia", "Reykjavik", "Kyoto", "Tromso"];
+vacayApp.hereURL = `https:weather.ls.hereapi.com/weather/1.0/report.json`;
+vacayApp.hereMapURL = `https://image.maps.ls.hereapi.com/mia/1.6/`;
+vacayApp.destinations = [
+    ["Bali", "You’ll find beaches, volcanoes, Komodo dragons and jungles sheltering elephants, orangutans and tigers. Basically, it’s paradise. It’s likely you’ve seen an image of Bali on social media at least once in the past seven days, as it’s such a popular bucket list destination for 2019. -forbes.com"], 
+    ["Kerry", "All the way west in Ireland is one of the country’s most scenic counties. Kerry’s mountains, lakes and coasts are postcard-perfect, and that’s before you add in Killarney National Park. The unique small towns such as Dingle add to its charm. -forbes.com"], 
+    ["Male", "This tropical nation in the Indian Ocean is made up of more than 1,000 coral islands. It’s home to some of the world’s most luxurious hotel resorts, with white sandy beaches, underwater villas and restaurants and bright blue waters. -forbes.com"],
+    ["Waikato", "Waikato, a region in New Zealand’s North Island, is home to massive underground caves, lush rainforest and the buzzy city of Hamilton. But the area’s main attraction? A Middle-earth adventure on the film set of Lord of the Rings. Hobbiton Movie Set still has the original Hobbit holes from the making of the films. -forbes.com"], 
+    ["Mcmurdo Station", "The McMurdo Station is a United States Antarctic research station on the south tip of Ross Island, which is in the New Zealand–claimed Ross Dependency on the shore of McMurdo Sound in Antarctica. It is operated by the United States through the United States Antarctic Program, a branch of the National Science Foundation. -wikipedia.org"],
+    ["Bridgetown", "Bridgetown, the capital of Barbados, is a port city on the island’s southwest coast. It's known for its British colonial architecture, 17th-century Garrison and horseracing track. Near the central National Heroes Square, which fringes the Constitution River, Nidhe Israel Synagogue and its museum explore the island’s Jewish history. -wikipedia.org"], 
+    ["Providencia", "Providencia is an ecologically important reserve that contains some of the world's greatest marine biodiversity, and incorporates the Archipelago of San Andrés, Providencia and Santa Catalina, in addition to the remote uninhabited cays at Roncador Bank, Serrana Bank, as well as distant reefs that include Quita Sueño Bank, Rosalind Bank and Alice Shoal. -wikipedia.org"], 
+    ["Reykjavik", "The first permanent settlement in Iceland by Norsemen is believed to have been established at Reykjavík by Ingólfr Arnarson around AD 870; this is described in Landnámabók, or the Book of Settlement. Ingólfur is said to have decided the location of his settlement using a traditional Norse method: he cast his high seat pillars (Öndvegissúlur) into the ocean when he saw the coastline, then settled where the pillars came to shore. -wikipedia.org"], 
+    ["Kyoto", "About 20% of Japan's National Treasures and 14% of Important Cultural Properties exist in the city proper. The UNESCO World Heritage Site Historic Monuments of Ancient Kyoto (Kyoto, Uji and Otsu Cities) includes 17 locations in Kyoto, Uji in Kyoto Prefecture, and Ōtsu in Shiga Prefecture. The site was designated as World Heritage in 1994. -wikipedia.org"], 
+    ["Tromso", "The city centre of Tromsø contains the highest number of old wooden houses in Northern Norway, the oldest house dating from 1789. The city is a cultural centre for its region, with several festivals taking place in the summer. Torbjørn Brundtland and Svein Berge of the electronica duo Röyksopp and Lene Marlin grew up and started their careers in Tromsø. Noted electronic musician Geir Jenssen also hails from Tromsø. -wikipedia.org"]];
 
 vacayApp.userSelect;
 
 // loop through destinations individually to feed into ajax
 vacayApp.destinationsCycle = () => {
     vacayApp.destinations.forEach((location) => {
-        vacayApp.getDestWeather(location);
-    });
-};
-
-// ajax call to run our cities through to get the array data from.
-vacayApp.getDestWeather = (input) => {
-    $.ajax({
-        url: vacayApp.hereURL,
-        method: "GET",
-        dataType: "json",
-        data: {
-            apiKey: vacayApp.hereApiKey,
-            product: "observation",
-            name: input
-        }
-    }).then((result) => {
-        const vacayArray = result.observations.location[0];
-        vacayApp.displayVacay(vacayArray, vacayApp.userSelect);
+        vacayApp.getDestWeather(location[0]);
     });
 };
 
@@ -100,6 +94,11 @@ vacayApp.displayVacay = (vacay, userSelection) => { //this needs to take data fr
     userSelection === "Cool" && tempDesc === "Chilly") {
         // console.log(`The weather in ${city}, ${country} is ${tempDesc}. The current high temperature is ${temp}.`);
         // need to append the above to the html when that portion has been completed
+        
+        vacayApp.mapPromise(city).then( (result) => {
+            // trying to display map image but the responseText is literally an image. don't know how to use it
+        });
+        
         const resultsHtml = `
             <button class="${city} ${country}">${city}, ${country}</button>
             <p>The current high temperature is ${temp}</p>`;
@@ -112,6 +111,38 @@ vacayApp.displayVacay = (vacay, userSelection) => { //this needs to take data fr
         console.log("FUUUUCCCK"); // error
     }
 
+}
+
+
+// ajax call to run our cities through to get the array data from.
+vacayApp.getDestWeather = (input) => {
+    $.ajax({
+        url: vacayApp.hereURL,
+        method: "GET",
+        dataType: "json",
+        data: {
+            apiKey: vacayApp.hereApiKey,
+            product: "observation",
+            name: input,
+            nocp: true
+        }
+    }).then((result) => {
+        const vacayArray = result.observations.location[0];
+        vacayApp.displayVacay(vacayArray, vacayApp.userSelect);
+    });
+};
+
+// maps promise
+vacayApp.mapPromise = (city) => {
+    return $.ajax({
+        url: vacayApp.hereMapURL,
+        method: "GET",
+        dataType: "json",
+        data: {
+            apiKey: vacayApp.hereApiKey,
+            ci: city
+        }
+    });
 }
 
 
